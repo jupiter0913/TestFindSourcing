0bdd28fc822fc2811778ddf371cc753f296385fc
jupiter0913 <jupiter0913@protonmail.com>
2020-09-02 20:19:30 +0200

    Add assets files.

---
+++ assets/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1 class="block__element--modifier">{{ world }}</h1>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        world: 'Hello!'
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .block {
+    &__element {
+      &--modifier {
+        color: green;
+      }
+    }
+  }
+</style>
